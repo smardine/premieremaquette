ba5477f9570165dd082c6da02fcce81275378c87
s.mardine <s.mardine@60d01b4f-27ba-c111-d7b1-4d48929f71a3>
2010-10-05 13:41:30 +0000

    rajout d'une notification en fin de telechargement

--- trunk/ premieremaquette --username s.mardine/src/fr/smardine/android/premiereMaquette/DownloadFile.java
+++ trunk/ premieremaquette --username s.mardine/src/fr/smardine/android/premiereMaquette/DownloadFile.java
@@ -7,13 +7,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+
+
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ProgressBar;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 
 public class DownloadFile extends Activity implements OnClickListener{
@@ -23,6 +34,8 @@ public class DownloadFile extends Activity implements OnClickListener{
 	int Pourcent=0;
 	long Vitesse=0;
 	String Status="";
+	NotificationManager notificationManager;
+	boolean NotifDejaLancée=false;
 	
 
 	   
@@ -37,7 +50,12 @@ public class DownloadFile extends Activity implements OnClickListener{
 	       // creation du thread qui va rafraichir les valeur de progression et de vitesse
 	        updateUI();
 	        handler.removeCallbacks(updateTimeTask);
-			handler.postDelayed(updateTimeTask, 1000);	 
+			handler.postDelayed(updateTimeTask, 1000);	
+			
+			//Notification est un service qui tourne en background et sert à interagir avec les notifications
+			notificationManager=(NotificationManager)this.getSystemService(Context.NOTIFICATION_SERVICE);
+		        //annuler toute notification précédente
+		    notificationManager.cancelAll();
 	       
 	}
 	 private Handler handler = new Handler();
@@ -59,10 +77,50 @@ public class DownloadFile extends Activity implements OnClickListener{
 			MessageVitesse.setText("Vitesse Actuelle : "+ Vitesse + " Ko/s");
 			Progress.setProgress(Pourcent);
 			MessageStatus.setText (Status);
-
+			if (Status.equals("Télèchargement terminé!!!")&&NotifDejaLancée==false){
+				lanceNotification();
+				NotifDejaLancée=true;
+			}
 			}
 		
 	
+	private void lanceNotification() {
+		// Créer un notification à l'action clic sur le bouton de l'activité Main
+		Notification nf = new Notification(R.drawable.eqx_icon,"Synchronisation terminée",System.currentTimeMillis());
+		// pour exécuter la notification il faut:
+		nf.contentView = new RemoteViews(getPackageName(), R.layout.note);
+		//contentView sera le contenu du message qui sera affiché dans la notification
+		//RemoteViews sont des vues qui ne sont pas rattachées à l'activité en cours
+		nf.icon = R.drawable.eqx_icon;
+		nf.vibrate = new long[]{0,100,25,100};
+		//indiquer quel fichier de son à jouer à réception de la notification
+		// le fichier son doit être de format .ogg
+		nf.sound = Uri.parse("android.resource://"+this.getPackageName()+"/"+R.raw.droid);  
+		nf.ledOffMS = 25; //en milliseconde quand la led du téléphone s'éteind
+		nf.ledOnMS = 100; // quand la led du téléphone va s'allumer
+		nf.ledARGB = Color.CYAN; // définir la couleur de la LED
+		nf.flags = nf.flags | Notification.FLAG_SHOW_LIGHTS;
+		
+		//contentIntent est une activité qui va se lancer lorsqu'on clic sur la notification qui apparait dans la barre de statut
+		//obligatoire à toute création de notification
+		Intent activity = new Intent(this, Note.class);
+		activity.putExtra("param", 1);
+		Log.i("", ">> Main, affecter l'activity à la visualisation de la notification");
+		PendingIntent pendingintent = PendingIntent.getActivity(this, 0, activity, 0);
+		nf.contentIntent = pendingintent;
+		
+		activity = new Intent(this, Note.class);
+		activity.putExtra("param", 2);
+		Log.i("", ">> Main, affecter l'activity à la suppression de la notification");
+		//il est possible d'appeler une activité lorsque la notification est supprimée
+		nf.deleteIntent = PendingIntent.getActivity(this, 0, activity, 0);
+		
+		//pour lancer la notification
+		notificationManager.notify(-1, nf);
+		
+	}
+
+
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 	}
@@ -76,7 +134,7 @@ public class DownloadFile extends Activity implements OnClickListener{
         super.onResume();
         handler.removeCallbacks(updateTimeTask);
         handler.postDelayed(updateTimeTask, 1000);
-        new DownloadFileTask().execute("http://downloads.sourceforge.net/project/carnetclient/setup/setupCarnetClient.exe");
+        new DownloadFileTask().execute("http://support.microconcept.fr/Outils%5CTeleMaintenance/PcHelpWare.exe");
        
     }
     
@@ -163,7 +221,7 @@ private class DownloadFileTask extends AsyncTask {
 				{	
 					Pourcent=0;
 					Vitesse=0;
-					Status="Telechargement terminé!!!";
+					Status="Télèchargement terminé!!!";
 					writeFile.close();
 					input.close();
 				}
