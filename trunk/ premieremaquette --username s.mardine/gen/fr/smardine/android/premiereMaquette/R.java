cca27d083cea63098257d0513da8785e27f1cb4d
s.mardine <s.mardine@60d01b4f-27ba-c111-d7b1-4d48929f71a3>
2010-10-06 14:29:34 +0000

    le telechargement de fichier se fait dans la carte sd dans le repertoire /sd/dl une notification se lance a la fin du telechargement ajout de la date/heure sur la page d'acceuil et suppression des icone equinox

--- trunk/ premieremaquette --username s.mardine/gen/fr/smardine/android/premiereMaquette/R.java
+++ trunk/ premieremaquette --username s.mardine/gen/fr/smardine/android/premiereMaquette/R.java
@@ -11,33 +11,41 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int client_icone96=0x7f020000;
-        public static final int eqx_icon=0x7f020001;
-        public static final int eqx_v_188x140=0x7f020002;
-        public static final int eqx_v_250x186=0x7f020003;
-        public static final int eqx_v_500x372=0x7f020004;
-        public static final int eqx_v_63x47=0x7f020005;
-        public static final int icon=0x7f020006;
-        public static final int parametres_96=0x7f020007;
-        public static final int piece_jointe96=0x7f020008;
-        public static final int synchroniser_96=0x7f020009;
-        public static final int voiture96=0x7f02000a;
+        public static final int calendrie_96=0x7f020000;
+        public static final int calendrier_128=0x7f020001;
+        public static final int calendrier_48=0x7f020002;
+        public static final int client_icone96=0x7f020003;
+        public static final int eqx_icon=0x7f020004;
+        public static final int eqx_v_188x140=0x7f020005;
+        public static final int eqx_v_250x186=0x7f020006;
+        public static final int eqx_v_500x372=0x7f020007;
+        public static final int eqx_v_63x47=0x7f020008;
+        public static final int icon=0x7f020009;
+        public static final int parametres_96=0x7f02000a;
+        public static final int piece_jointe96=0x7f02000b;
+        public static final int synchroniser_96=0x7f02000c;
+        public static final int voiture96=0x7f02000d;
     }
     public static final class id {
-        public static final int BTPieceJointe=0x7f070004;
-        public static final int BtClient=0x7f070003;
-        public static final int BtGo=0x7f07000c;
-        public static final int BtParam=0x7f070005;
-        public static final int BtTournee=0x7f070006;
-        public static final int EditTextIdUtilisateur=0x7f07000a;
-        public static final int EditTextPassword=0x7f07000b;
-        public static final int ImgEqnx=0x7f070001;
+        public static final int BTPieceJointe=0x7f070005;
+        public static final int BtClient=0x7f070004;
+        public static final int BtGo=0x7f07000e;
+        public static final int BtParam=0x7f070006;
+        public static final int BtTournee=0x7f070007;
+        public static final int DigitalClock01=0x7f07000a;
+        public static final int EditTextIdUtilisateur=0x7f07000c;
+        public static final int EditTextPassword=0x7f07000d;
+        public static final int ImgCalendr=0x7f070001;
+        public static final int Layout_FicheClient=0x7f070000;
         public static final int Nomdupatient=0x7f070002;
-        public static final int ProgressBar01=0x7f070009;
-        public static final int RelativeLayout01=0x7f07000d;
-        public static final int TxtParam=0x7f070007;
-        public static final int text=0x7f070008;
-        public static final int widget63=0x7f070000;
+        public static final int ProgressBar01=0x7f070010;
+        public static final int RelativeLayout01=0x7f07000f;
+        public static final int TextView01=0x7f070011;
+        public static final int TextView02=0x7f070012;
+        public static final int TexteDate=0x7f070009;
+        public static final int TxtParam=0x7f070008;
+        public static final int text=0x7f07000b;
+        public static final int widget63=0x7f070003;
     }
     public static final class layout {
         public static final int fiche_client=0x7f030000;
